196ddb6822ef7949e5c3b749a829d326fce1d648
Mohit yadav <y2641699@gmail.com>
2025-01-23 09:03:32 +0530

    string dsa que

---
+++ Longest substring without repeating chararcters.py
@@ -0,0 +1,38 @@
+# Example 1:
+
+# Input: s = "abcabcbb"
+# Output: 3
+# Explanation: The answer is "abc", with the length of 3.
+# Example 2:
+
+# Input: s = "bbbbb"
+# Output: 1
+# Explanation: The answer is "b", with the length of 1.
+# Example 3:
+
+# Input: s = "pwwkew"
+# Output: 3
+# Explanation: The answer is "wke", with the length of 3.
+# Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        l = 0
+        longest = 0
+        sett = set()
+        n = len(s)
+
+        for r in range(n):
+            while s[r] in sett:
+                sett.remove(s[l])
+                l += 1
+            
+            w = (r - l) + 1
+            longest = max(longest, w)
+            sett.add(s[r])
+
+        return longest
+
+        # O(n)
+        # O(n)
