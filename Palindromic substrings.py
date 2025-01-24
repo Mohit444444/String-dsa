9746a48d2337a4e278fab708cce9850f8ee76215
Mohit yadav <y2641699@gmail.com>
2025-01-24 09:44:07 +0530

    string dsa que

--- Palindromic substrings.py
+++ Palindromic substrings.py
@@ -1,8 +1,32 @@
-Input: s = "abc"
-Output: 3
-Explanation: Three palindromic strings: "a", "b", "c".
-Example 2:
+# Input: s = "abc"
+# Output: 3
+# Explanation: Three palindromic strings: "a", "b", "c".
+# Example 2:
 
-Input: s = "aaa"
-Output: 6
-Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+# Input: s = "aaa"
+# Output: 6
+# Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        res = 0
+        
+        for i in range(len(s)):
+            # odd length
+            l, r = i, i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                res += 1
+                l -= 1
+                r += 1
+
+            # even length
+            l, r = i, i + 1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                res += 1
+                l -= 1
+                r += 1
+        
+        return res
+
+        # time O(n^2)
+        # space O(1)
